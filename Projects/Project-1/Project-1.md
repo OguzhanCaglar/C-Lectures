0b47cc7df5b827f99ee63773d0f4f19ba262c5ed
Oğuzhan Çağlar <oguzhan.caglar1@outlook.com>
2025-05-18 16:16:50 +0300

    feat: Implemented new features: - Delete Note which specified - Password Entry - Change password

--- Projects/Project-1/Project-1.md
+++ Projects/Project-1/Project-1.md
@@ -14,10 +14,19 @@
 - Yeni not ekleme --> +
 - Tüm notları görüntüle --> +
 - Çıkış yap --> +
-- Tarih-Saat Ekleme: Her notun başına zaman damgası koyalım.
-- Not arama
+- Tarih-Saat Ekleme: Her notun başına zaman damgası koyalım. --> +
+- Not arama --> +
+- Belirli Notu Silme Özelliği --> +
+- Sifreli giriş --> +
+- Sifre Değiştirme --> +
+
 
 **Eklenecek Özellikler**:
-- Belirli Notu Silme Özelliği
+- Kategori / Etiket Sistemi:
+    - Kullanıcı not eklerken bir kategori girer (örneğin: iş, okul, alışveriş)
+    - Not, kategori ile birlikte dosyaya kaydedilir.
+    - Kullanıcı isterse sadece belirli kategoriye ait notları görüntüleyebilir veya arayabilir.
+    - [kisisel] [12-07-2025 20:10] Markete git.
+    - [okul] [12-07-2025 10.10] Derin Ogrenme Notlarını düzenledim.
 
 Notepad.c
--- Projects/Project-1/notepad.c
+++ Projects/Project-1/notepad.c
@@ -5,6 +5,94 @@
 
 #define FILENAME            "notes.txt"
 #define MAX_NOTE_LENGTH     256
+#define EXIT_COMMAND        6
+#define PASSWORD_FILE       "password.txt"
+#define MAX_ATTEMPS         3
+
+/* ================ PASSWORD OPERATIONS ===============*/
+
+int checkPassword(void)
+{
+    FILE *fp = fopen(PASSWORD_FILE, "r");
+    if(fp == NULL)
+    {
+        printf("HATA: %s dosyasi bulunamadi!\n", PASSWORD_FILE);
+        return 0;
+    }
+
+    char realPassword[50];
+    fgets(realPassword, sizeof(realPassword), fp);
+    realPassword[strcspn(realPassword, "\n")] = '\0';
+    fclose(fp);
+
+    char input[50];
+    int attempts = 0;
+
+    while (attempts < MAX_ATTEMPS)
+    {
+        printf("Sifreyi girin (%d/%d): ", attempts + 1, MAX_ATTEMPS); // Sifreyi girin (1/3);
+        scanf("%s", input);
+
+        if(strcmp(input, realPassword) == 0)
+        {
+            printf("Giris basarili! Hos geldiniz!\n\n");
+            return 1;
+        } 
+        else 
+        {
+            printf("Hatali Sifre. %d hakkiniz kaldi.\n", MAX_ATTEMPS - attempts - 1);
+            attempts++;
+        }
+    }
+
+
+    printf("3 yanlis giris yapildi. Program sonlandiriliyor...\n");
+    return 0;    
+}
+
+void changePassword()
+{
+    FILE *fp = fopen(PASSWORD_FILE, "r");
+    if(fp == NULL)
+    {
+        printf("Sifre dosyasi acilamadi!\n");
+        return;
+    }
+
+    char currentPassword[50];
+    fgets(currentPassword, sizeof(currentPassword), fp);
+    currentPassword[strcspn(currentPassword, "\n")] = '\0'; // newline sil
+    fclose(fp);
+
+    char input[50];
+    printf("Mevcut sifreyi girin: ");
+    scanf("%s", input);
+
+    if(strcmp(input, currentPassword) != 0)
+    {
+        printf("Hatali sifre. Sifre degistirilemedi!\n");
+        return;
+    }
+
+    char newPassword[50];
+    printf("Yeni sifreyi girin: ");
+    scanf("%s", newPassword);
+
+    fp = fopen(PASSWORD_FILE, "w");
+    if(fp == NULL)
+    {
+        printf("Yeni sifre yazilamadi.\n");
+        return;
+    }
+
+    fprintf(fp, "%s\n", newPassword);
+    fclose(fp);
+
+    printf("Sifre basariyla degistirildi.\n");
+}
+
+
+/* ================ NOTE OPERATIONS ===============*/
 
 void addNote(void)
 {
@@ -90,8 +178,74 @@ void searchNotes(void)
     fclose(fp);
 }
 
+void deleteNote() 
+{
+    FILE *fp = fopen(FILENAME, "r");
+    if(fp == NULL) 
+    {
+        printf("Not dosyasi bulunamadi.\n");
+        return;
+    }
+
+    // Tüm notları oku ve ekrana numaralı yaz.
+    char lines[100][MAX_NOTE_LENGTH];
+    int count = 0;
+
+    while (fgets(lines[count], MAX_NOTE_LENGTH, fp))
+    {
+        printf("%d. %s", count + 1, lines[count]);
+        count++;
+    }
+    fclose(fp);
+
+    if(count == 0)
+    {
+        printf("Silinecek not bulunamadi.\n\n");
+    }
+
+    // Kullanıcıdan silinecek satır numarasını alalım.
+    int choice;
+    printf("\nSilmek istediginiz not numarasini girin: ");
+    scanf("%d", &choice);
+    
+    if(choice < 1 || choice > count)
+    {
+        printf("Gecersiz secim.\n\n");
+        return;
+    }
+
+    // Yeni dosyaya yeniden yaz (seçilen hariç)
+    FILE *fp2 = fopen("temp.txt", "w");
+    if(fp2 == NULL)
+    {
+        printf("Gecici dosya olusturulamadi\n");
+        return;
+    }
+
+    for(int i = 0; i < count; i++)
+    {
+        if( i != choice - 1)
+        {
+            fputs(lines[i], fp2);
+        }
+    }
+
+    fclose(fp2);
+
+    // temp.tx --> notes.txt
+    remove(FILENAME);
+    rename("temp.txt", FILENAME);
+
+    printf("Not silindi.\n\n");
+}
+
 int main()
 {   
+    if(!checkPassword())
+    {
+        return 1; // Sifre yanlis oldugundan programdan cikiliyor.
+    }    
+    
     int choice;
 
     do
@@ -100,7 +254,9 @@ int main()
         printf("1. Not Ekle\n");
         printf("2. Notlari Goruntule\n");
         printf("3. Not Ara\n");
-        printf("4. Cikis\n");
+        printf("4. Not Sil\n");
+        printf("5. Sifre Degistir\n");
+        printf("%d. Cikis\n", EXIT_COMMAND);
         printf("Seciminiz: ");
         scanf("%d", &choice);
 
@@ -117,15 +273,23 @@ int main()
             case 3:
                 searchNotes();
                 break;
-            
+
             case 4:
+                deleteNote();
+                break;
+            
+            case 5:
+                changePassword();
+                break;
+            
+            case EXIT_COMMAND:
                 printf("Programdan cikiliyor...\n");
                 break;
             
             default:
                 printf("Gecersiz Secim!\n");
         }
-    } while (choice != 3);
+    } while (choice != EXIT_COMMAND);
     
 
     return 0;
